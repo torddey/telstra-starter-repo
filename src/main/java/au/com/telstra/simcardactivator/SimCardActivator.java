6e2b54627a28cb0ad0bcfafc94097f66af4cc78c
torddey <ea.torddey@gmail.com>
2024-10-16 22:17:29 +0000

    init

--- src/main/java/au/com/telstra/simcardactivator/SimCardActivator.java
+++ src/main/java/au/com/telstra/simcardactivator/SimCardActivator.java
@@ -6,8 +6,17 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SimCardActivator {
 
+    private static final String ACTUATOR_URL = 'http://localhost:8444/actuate';
+
     public static void main(String[] args) {
         SpringApplication.run(SimCardActivator.class, args);
     }
 
+}
+
+@PostMapping("activate")
+public String activateSim(@RequestBody ActivationRequest request) {
+    RestTemplate restTemplate = new RestTemplate();
+
+    
 }
