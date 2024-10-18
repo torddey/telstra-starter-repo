a4c20a1fa392c0f8b736fe9b486a7807d0a418a6
torddey <ea.torddey@gmail.com>
2024-10-18 13:05:54 +0000

    Update API Endpoints

--- src/main/java/au/com/telstra/simcardactivator/SimCardActivator.java
+++ src/main/java/au/com/telstra/simcardactivator/SimCardActivator.java
@@ -2,6 +2,11 @@ package au.com.telstra.simcardactivator;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
 
 @SpringBootApplication
 public class SimCardActivator {
@@ -12,11 +17,40 @@ public class SimCardActivator {
         SpringApplication.run(SimCardActivator.class, args);
     }
 
+
+    @PostMapping("activate")
+    public String activateSim(@RequestBody ActivationRequest request) {
+        RestTemplate restTemplate = new RestTemplate();
+
+        ActuatorRequest actuatorRequest = new ActuatorRequest(request.getIccid());
+        ActuatorResponse actuatorResponse = restTemplate.postForObject(ACTUATOR_URL, actuatorRequest, ActuatorResponse.class);
+
+        boolean sucess = actuatorRequest = new ActuatorRequest(request.getIccid());
+        System.out.printIn("SIM activation " + (sucess ? "successful" : "failed") + "for ICCID: " + request.getIccid());
+
+        return "Activation request processed. Success: " + sucess;
+    }
 }
 
-@PostMapping("activate")
-public String activateSim(@RequestBody ActivationRequest request) {
-    RestTemplate restTemplate = new RestTemplate();
+class ActivationRequest {
+    private String Iccid;
+    private String customerEmail;
 
-    
+    public String getIccid() { return Iccid; }
+    public void setIccid(String Iccid) { this.Iccid = Iccid; }
+    public String getCustomerEmail() { return customerEmail; }
+    public void setCustomerEmail(String customerEmail) { this.customerEmail = customerEmail; }
+}
+
+class ActuatorRequest {
+    private String Iccid;
+    public ActuatorRequest(String Iccid) { this.Iccid = Iccid; }
+    public String getIccid() { return Iccid; }
+    public void setIccid(String iccid) { this.iccid = iccid; }
+}
+
+class ActuatorResponse {
+    private boolean sucess;
+    public boolean isSuccess() { return sucess; }
+    public void setSucess(boolean success) { this.sucess = success; }
 }
